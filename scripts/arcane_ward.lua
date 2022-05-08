ed73f347e7761554297d90b4771274e636906d07
Ryan Hagelstrom <rhagelstrom@gmail.com>
2022-05-08 15:23:22 -0500

    effect for reactions

--- scripts/arcane_ward.lua
+++ scripts/arcane_ward.lua
@@ -18,13 +18,12 @@
 local applyDamage = nil
 local messageDamage = nil
 local rest = nil
-
+local addNPCtoCT = nil
 --TODO:
 -- Button to cast abjuration spells
 -- parse NPC for the text "X hit points" and that is it's ward #
 -- CT and Char sheet boxes to display current AW HP
 -- Upcast
--- AW Effect for reactions
 
 OOB_MSGTYPE_ARCANEWARD = "arcaneward"
 
@@ -34,24 +33,29 @@ function onInit()
 	applyDamage = ActionDamage.applyDamage
 	messageDamage = ActionDamage.messageDamage
 	rest = CharManager.rest
+	addNPCtoCT= CombatManager.addNPC
 
 	ActionDamage.applyDamage = customApplyDamage
 	ActionDamage.messageDamage = customMessageDamage
 	CharManager.rest = customRest
+	CombatManager.addNPC = customAddNPCtoCT
+
 end
 
 function onClose()
 	ActionDamage.applyDamage = applyDamage
 	ActionDamage.messageDamage = messageDamage
 	CharManager.rest = rest
+	CombatManager.addNPC = addNPCtoCT
 end
 
 function castAbjuration(nodeActor, nLevel)
 	local nActive = DB.getValue(nodeActor, "arcaneward", 0)
+	local sDBAWHP = getDBString(nodeActor)
 	local nTotal
 
 	if nActive == 1 then
-		local nArcaneWardHP = DB.getValue(nodeActor, "hp.arcaneward", 0)
+		local nArcaneWardHP = DB.getValue(nodeActor, sDBAWHP, 0)
 		nTotal = nArcaneWardHP + nLevel * 2
 	else
 		local nBonus = DB.getValue(nodeActor, "abilities.intelligence.bonus", 0)
@@ -65,16 +69,28 @@ function castAbjuration(nodeActor, nLevel)
 		nTotal = nWizLevel * 2  + nBonus
 		DB.setValue(nodeActor, "arcaneward", "number", 1)
 	end
-	DB.setValue(nodeActor, "hp.arcaneward", "number", nTotal)
+	DB.setValue(nodeActor, sDBAWHP, "number", nTotal)
 end
 
 function hasArcaneWard(rActor)
 	local nodeActor = ActorManager.getCreatureNode(rActor)
 	local nodeFeatures = nodeActor.getChild("featurelist")
-	if nodeFeatures ~= nil then
+	--PCs
+	if nodeFeatures ~= nil and (rActor.sType == "pc" or rActor.sType == "charsheet") then
 		local aFeatures = nodeFeatures.getChildren()
 		for _, nodeFeature in pairs(aFeatures) do
 			local sName = DB.getValue(nodeFeature, "name", "")
+			if sName:upper() == "ARCANE WARD" then
+				return true
+			end
+		end
+	end
+	--NPCs
+	local nodeTraits = nodeActor.getChild("traits")
+	if nodeTraits ~= nil and rActor.sType == "npc" then
+		local aTraits = nodeTraits.getChildren()
+		for _, nodeTrait in pairs(aTraits) do
+			local sName = DB.getValue(nodeTrait, "name", "")
 			if sName:upper() == "ARCANE WARD" then
 				return true
 			end
@@ -86,7 +102,9 @@ end
 function arcaneWard(rSource, rTarget, bSecret, sDamage, nTotal)
 	local nodeTarget = ActorManager.getCreatureNode(rTarget)
 	local nActive = DB.getValue(nodeTarget, "arcaneward", 0)
-	local nArcaneWardHP = DB.getValue(nodeTarget, "hp.arcaneward", 0)
+	local sDBAWHP = getDBString(nodeTarget)
+--	Debug.chat("ArcaneWard: " .. sDBAWHP)
+	local nArcaneWardHP = DB.getValue(nodeTarget, sDBAWHP, 0)
 
 	if nActive == 1 and nArcaneWardHP > 0 then
 		local nTotalOrig = nTotal
@@ -97,7 +115,7 @@ function arcaneWard(rSource, rTarget, bSecret, sDamage, nTotal)
 			nArcaneWardHP = nArcaneWardHP - nTotal
 			nTotal = 0
 		end
-		DB.setValue(nodeTarget, "hp.arcaneward", "number", nArcaneWardHP)
+		DB.setValue(nodeTarget, sDBAWHP, "number", nArcaneWardHP)
 		sDamage = removeAbsorbed(sDamage, nTotalOrig -  nTotal)
 		sDamage =  "[ARCANE WARD: " .. tostring(nTotalOrig - nTotal) .. "] " .. sDamage
 	end
@@ -132,16 +150,29 @@ function removeAbsorbed(sDamage, nAbsorbed)
 	return sNewDamage
 end
 
+function getDBString(node)
+	local rActor = ActorManager.resolveActor(node)
+	if ActorManager.isPC(rActor) then
+		return "hp.arcaneward"
+	else
+		return "arcanewardhp"
+	end
+end
+
 
 function customApplyDamage (rSource, rTarget, bSecret, sDamage, nTotal)
-	local ctEntries = CombatManager.getCombatantNodes()
-	for _, nodeCT in pairs(ctEntries) do
-		local rActor = ActorManager.resolveActor(nodeCT)
-		if rActor ~= rSource and hasArcaneWard(rActor) then
-			local aArcaneWard = EffectManager5E.getEffectsByType(rTarget, "ARCANEWARD", {}, rActor)
-			if aArcaneWard ~= {} then
-				--Technically you could have two different wizards spend their reaction on this character
-				sDamage, nTotal = arcaneWard(rSource, rActor, bSecret, sDamage, nTotal)
+	-- Get the effects on source, determine. is arcane ward. determine source
+	local aArcaneWardEffects = getEffectsByType(rTarget, "ARCANEWARD")
+	if next(aArcaneWardEffects) then
+		local ctEntries = CombatManager.getCombatantNodes()
+		for _, nodeCT in pairs(ctEntries) do
+			local rActor = ActorManager.resolveActor(nodeCT)
+			if hasArcaneWard(rActor) then
+				for _, rEffect in pairs(aArcaneWardEffects) do
+					if rEffect.source_name == rActor.sCTNode then
+						sDamage, nTotal = arcaneWard(rSource, rActor, bSecret, sDamage, nTotal)
+					end
+				end
 			end
 		end
 	end
@@ -172,6 +203,101 @@ function customRest(nodeActor, bLong)
 	rest(nodeActor, bLong)
 end
 
+
+function customAddNPCtoCT(sClass, nodeNPC, sName)
+	local nodeCTEntry  = addNPCtoCT(sClass, nodeNPC, sName)
+	local nodeFeatures = nodeCTEntry.getChild("traits")
+	local aFeatures = nodeFeatures.getChildren()
+	for _, nodeFeature in pairs(aFeatures) do
+		local sFeatureName = DB.getValue(nodeFeature, "name", "")
+		if sFeatureName:upper() == "ARCANE WARD" then
+			local sDesc =  DB.getValue(nodeFeature, "desc", "")
+			local aWords = StringManager.parseWords(sDesc)
+			local nArcaneWard = 0
+			local i = 1
+			while aWords[i] do
+				if StringManager.isWord(aWords[i], "hit") and StringManager.isWord(aWords[i+1], "points") then
+					nArcaneWard = tonumber(aWords[i-1])
+					DB.setValue(nodeCTEntry, "arcaneward", "number", 1)
+					DB.setValue(nodeCTEntry, "arcanewardhp", "number", nArcaneWard)
+					break
+				end
+				i = i + 1
+			end
+		end
+	end
+	return nodeCTEntry
+end
+
+--Modified from coreRPG to also return the CTNode whom applied the effect
+--5E version is too bloated for what we need
+function getEffectsByType(rActor, sEffectCompType, rFilterActor, bTargetedOnly)
+	if not rActor then
+		return {};
+	end
+	local tResults = {};
+	local tEffectCompParams = {}
+	tEffectCompParams[sEffectCompType] = {}
+	-- Iterate through effects
+	for _,v in pairs(ActorManager.getEffects(rActor)) do
+		-- Check active
+		local nActive = DB.getValue(v, "isactive", 0);
+		local bActive =
+				(tEffectCompParams.bIgnoreExpire and (nActive == 1)) or
+				(not tEffectCompParams.bIgnoreExpire and (nActive ~= 0));
+
+		if bActive then
+			-- If effect type we are looking for supports targets, then check targeting
+			local bTargetMatch = false;
+			if tEffectCompParams.bIgnoreTarget then
+				bTargetMatch = true;
+			else
+				local bTargeted = EffectManager.isTargetedEffect(v);
+				if bTargeted then
+					bTargetMatch = EffectManager.isEffectTarget(v, rFilterActor);
+				else
+					bTargetMatch = not bTargetedOnly;
+				end
+			end
+
+			if bTargetMatch then
+				local sLabel = DB.getValue(v, "label", "");
+				local aEffectComps = EffectManager.parseEffect(sLabel);
+				-- Look for type/subtype match
+				local nMatch = 0;
+				for kEffectComp,sEffectComp in ipairs(aEffectComps) do
+					local rEffectComp = EffectManager.parseEffectCompSimple(sEffectComp);
+					if rEffectComp.type:upper() == sEffectCompType or rEffectComp.original:upper() == sEffectCompType then
+						nMatch = kEffectComp;
+						if nActive == 1 then
+							rEffectComp.source_name = DB.getValue(v, "source_name", "");
+							table.insert(tResults, rEffectComp);
+						end
+					end
+				end -- END EFFECT COMPONENT LOOP
+
+				-- Remove one shot effects
+				if (nMatch > 0) and not tEffectCompParams.bIgnoreExpire then
+					if nActive == 2 then
+						DB.setValue(v, "isactive", "number", 1);
+					else
+						local sApply = DB.getValue(v, "apply", "");
+						if sApply == "action" then
+							EffectManager.notifyExpire(v, 0);
+						elseif sApply == "roll" then
+							EffectManager.notifyExpire(v, 0, true);
+						elseif sApply == "single" then
+							EffectManager.notifyExpire(v, nMatch, true);
+						end
+					end
+				end
+			end -- END TARGET CHECK
+		end  -- END ACTIVE CHECK
+	end  -- END EFFECT LOOP
+
+	-- RESULTS
+	return tResults;
+end
 function sendOOB(nodeActor,nLevel)
 	local msgOOB = {}
 	msgOOB.sNodeActor = nodeActor
